a49ee4997a20369793aadbca09c20142e49a4834
Raghav-dev <rhk4133@gmail.com>
2025-12-17 16:46:45 +0530

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-// OFFERS DATA
+// OFFERS DATA (USE AFFILIATE LINKS HERE)
 const offers = [
   {
     title: "iQOO Z9 5G",
     price: "₹17,999",
     platform: "Amazon",
-    url: "https://www.amazon.in"
+    url: "https://amzn.to/your_affiliate_code"
   },
   {
     title: "Realme Buds Air",
     price: "₹2,499",
     platform: "Flipkart",
-    url: "https://www.flipkart.com"
+    url: "https://fkrt.it/your_affiliate_code"
   },
   {
     title: "Redmi Power Bank",
     price: "₹1,199",
     platform: "Amazon",
-    url: "https://www.amazon.in"
+    url: "https://amzn.to/your_affiliate_code"
   }
 ];
 
 // LOAD OFFERS
-function loadOffers() {
+function loadOffers(filter = "All") {
   const container = document.getElementById("offers");
+  container.innerHTML = "";
 
   offers.forEach(offer => {
+    if (filter !== "All" && offer.platform !== filter) return;
+
     const div = document.createElement("div");
     div.className = "offer";
 
